88d0d361eb6a22e3264c90add6fe49946d1edcb3
Thomas <andrioletti.thomas@yahoo.com>
2025-05-23 13:36:19 +0200

    Test php 6

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ $ldap = ldap_connect("ldaps://192.168.100.6");
 ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
 ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
 
-if (@ldap_bind($ldap, "administrateur@fashionchic.local", "Windows2022")) {
+if (@ldap_bind($ldap, "ldap@fashionchic.local", "Windows2022")) {
     echo "Bind successful";
 } else {
     echo "Bind failed";
