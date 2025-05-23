834a6d1caf1c958ee9957dbe2195781f92c7d978
Thomas <andrioletti.thomas@yahoo.com>
2025-05-23 14:09:28 +0200

    Php login

--- login.php
+++ login.php
@@ -1,11 +1,23 @@
 <?php
-$ldap = ldap_connect("ldaps://192.168.100.6");
+putenv('LDAPTLS_CACERT=/etc/ssl/certs/CA-Root-fashionchic.crt');
+
+$ldap_host = "ldaps://ldaps.fashionchic.local";
+$ldap_port = 636;
+
+$ldap_user = "ldap@fashionchic.local";  
+$ldap_pass = "Windows2022";        
+
+// Connexion LDAP
+$ldap = ldap_connect($ldap_host, $ldap_port);
 ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
 ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
 
-if (@ldap_bind($ldap, "ldap@fashionchic.local", "Windows2022")) {
+// Tentative de bind
+if (@ldap_bind($ldap, $ldap_user, $ldap_pass)) {
     echo "Bind successful";
 } else {
-    echo "Bind failed";
+    echo "Bind failed<br>";
+    echo "LDAP error: " . ldap_error($ldap) . "<br>";
+    echo "LDAP errno: " . ldap_errno($ldap) . "<br>";
 }
 ?>
